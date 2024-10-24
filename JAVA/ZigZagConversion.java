ea374ed0854685e05614d4fa0ad43c1b846a693e
Aditya Tiwari <adityatiwari342005@gmail.com>
2024-10-24 19:16:21 +0530

    Added new file for ZigZag Conversion leetcode problem number 6
    In this Commit i have created and verified code for a popular ZigZag Conversion problem on leetcode.
    I have tested and checked the code working it is an optimal code.

---
+++ JAVA/ZigZagConversion.java
@@ -0,0 +1,41 @@
+import java.util.*;
+// Leetcode problem number 6
+public class ZigZagConversion{
+    public static String convert(String s, int numRows) {
+       if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        int idx = 0, d = 1;
+        List<Character>[] rows = new ArrayList[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new ArrayList<>();
+        }
+
+        for (char c : s.toCharArray()) {
+            rows[idx].add(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (char c : row) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();        
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int n= 3;
+        System.out.println(convert(s,n));
+    }
+
+}
